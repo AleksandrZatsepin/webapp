34771d4ba13a2cc06c6afd8be7b60e1978cf34bd
Aleksandr Zatsepin <mujustic@gmail.com>
2023-03-18 16:27:35 +0300

    Заготовка с рабочего

---
+++ src/main/java/web/controller/UsersController.java
@@ -0,0 +1,21 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import web.service.UserService;
+
+@Controller
+public class UsersController {
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public String printUsers() {
+        return null;
+    }
+}
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDao;
+
+@Service
+public class UserServiceImpl implements UserService{
+    private final UserDao userDao;
+
+
+    @Autowired
+    public UserServiceImpl (UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+}
